7871b5f77f0e4ed95ab34d733a52399d125c75d1
raubwuerger <hannuschka@web.de>
2022-08-05 15:49:16 +0200

    Changed content.

--- WeThePeople_ModdingTool/WeThePeople_ModdingTool/templates/Assets/Python/EntryPoints/CvRandomEventInterface_End_Template.py
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/templates/Assets/Python/EntryPoints/CvRandomEventInterface_End_Template.py
@@ -1,16 +1,3 @@
-def canTrigger$NATION_NORMAL$TradeQuest_$YIELD$_START(argsList):
-	
-	# Read Parameters 1+2 from the two events and check if enough yield is stored in city
-	eEvent = gc.getInfoTypeForString("EVENT_$NATION_UPPERCASE$_TRADE_QUEST_$YIELD$_START")
-	event = gc.getEventInfo(eEvent)
-	iYieldID = event.getGenericParameter(2)
-	iQuantity = event.getGenericParameter(1) # for Quest Start this should be e.g. 200
-
-	# Now we call the Generic Helper Function
-	bTrigger = CanDo$NATION_NORMAL$Trade(argsList, iYieldID, iQuantity)
-	
-	return bTrigger
-
 def canTrigger$NATION_NORMAL$TradeQuest_$YIELD$_DONE(argsList):
 	
 	# Read Parameters 1+2 from the two events and check if enough yield is stored in city
