95e162946737630f3c23259da10afb280c2910dd
Jürgen Hannuschka <juergen.hannuschka@sar.biz>
2022-08-08 10:27:35 +0200

    Added new classes.

---
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/EventCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace WeThePeople_ModdingTool
+{
+    public class EventCreator
+    {
+        public bool PreCreateEvent(XmlDocument document )
+        {
+            return false;
+        }
+    }
+}
---
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/TemplateRegistry.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using WeThePeople_ModdingTool.FileUtilities;
+
+namespace WeThePeople_ModdingTool
+{
+    public sealed class TemplateRegistry
+    {
+
+        private static readonly TemplateRegistry instance = new TemplateRegistry();
+
+        static TemplateRegistry()
+        {
+        }
+
+        public static TemplateRegistry Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private IDictionary<string, XmlDocument> xmlDocuments = new Dictionary<string, XmlDocument>();
+        private IDictionary<string, string> pythonFiles = new Dictionary< string, string>();
+
+        public bool RegisterTemplate( string name, XmlDocument document )
+        {
+            try
+            {
+                if (true == xmlDocuments.ContainsKey(name))
+                {
+                    return false;
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                CommonMessageBox.Show_OK_Error("Wrong parameter!","Key must not be null!");
+                return false;
+            }
+
+            try
+            {
+                return xmlDocuments.TryAdd(name, document);
+            }
+            catch( ArgumentNullException argumentNullException)
+            {
+                CommonMessageBox.Show_OK_Error("Wrong parameter!", "Key must not be null!");
+                return false;
+            }
+        }
+
+        public bool RegisterTemplate(string name, string pythonContent)
+        {
+            return false;
+        }
+    }
+}
