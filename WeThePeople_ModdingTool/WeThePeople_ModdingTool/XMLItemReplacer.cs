389fe5c45558b82ce5225afaf75a455561ebdf4d
Raubwuerger <hannuschka@web.de>
2022-08-11 18:56:49 +0200

    Further development.

--- WeThePeople_ModdingTool/WeThePeople_ModdingTool/XMLItemReplacer.cs
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/XMLItemReplacer.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
 namespace WeThePeople_ModdingTool
 {
-    class XMLItemReplacer
+    public class XMLItemReplacer
     {
+        private IDictionary<string, string> replaceItems = new Dictionary<string, string>();
+        public IDictionary<string, string> ReplaceItems
+        {
+            get { return replaceItems; }
+            set { replaceItems = value; }
+        }
+        public bool ReplaceItem( XmlDocument xmlDocument )
+        {
+            if( null == xmlDocument )
+            {
+                return false;
+            }
+
+            if( true == ReplaceItems.Count <= 0 )
+            {
+                return false;
+            }
+
+            return false;
+        }
     }
 }
