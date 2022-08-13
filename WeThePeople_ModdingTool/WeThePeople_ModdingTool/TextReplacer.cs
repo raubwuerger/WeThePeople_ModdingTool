c327c68d5e652c59c64418ddc389ebf991569b46
Raubwuerger <hannuschka@web.de>
2022-08-13 19:31:53 +0200

    Added class TextReplacer.

---
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/TextReplacer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WeThePeople_ModdingTool.FileUtilities;
+
+namespace WeThePeople_ModdingTool
+{
+    class TextReplacer
+    {
+        public static string replace( string content, KeyValuePair<string, string> replaceItem )
+        {
+            StringBuilder builder = new StringBuilder(content);
+            try
+            {
+                builder.Replace(replaceItem.Key, replaceItem.Value);
+                return builder.ToString();
+            }
+            catch( Exception ex )
+            {
+                CommonMessageBox.Show_OK_Error(CommonVariables.MESSAGE_BOX_EXCEPTION, "Replacing text failed!" + CommonVariables.CR + ex.Message);
+                return null;
+            }
+        }
+    }
+}
