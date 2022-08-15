00eff4f04e8d300230c91d64d61681799aa2ab6f
juergen.hannuschka <juergen.hannuschka@outlook.de>
2022-08-15 22:31:26 +0200

    Increased indention to 4.

--- WeThePeople_ModdingTool/WeThePeople_ModdingTool/TextReplacer.cs
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/TextReplacer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using WeThePeople_ModdingTool.FileUtilities;
+using Serilog;
 
 namespace WeThePeople_ModdingTool
 {
@@ -9,11 +10,14 @@ namespace WeThePeople_ModdingTool
     {
         public static string replace( string content, KeyValuePair<string, string> replaceItem )
         {
+            Log.Debug("Content to replace : " + content);
             StringBuilder builder = new StringBuilder(content);
             try
             {
                 builder.Replace(replaceItem.Key, replaceItem.Value);
-                return builder.ToString();
+                string contentReplaced = builder.ToString();
+                Log.Debug("Content replaced : " + contentReplaced);
+                return contentReplaced;
             }
             catch( Exception ex )
             {
@@ -26,11 +30,14 @@ namespace WeThePeople_ModdingTool
             StringBuilder builder = new StringBuilder(content);
             try
             {
+                Log.Debug("Content to replace : " + content);
                 foreach (KeyValuePair<string, string> entry in replaceItems)
                 {
                     builder.Replace(entry.Key, entry.Value);
                 }
-                return builder.ToString();
+                string contentReplaced = builder.ToString();
+                Log.Debug("Content replaced : " + contentReplaced);
+                return contentReplaced;
             }
             catch (Exception ex)
             {
