a8d20776a87f105aa2c524de5b27027e320d85eb
Raubwuerger <hannuschka@web.de>
2022-09-10 19:21:15 +0200

    Added assembly informations.

--- WeThePeople_ModdingTool/WeThePeople_ModdingTool/AssemblyInfo.cs
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/AssemblyInfo.cs
@@ -1,5 +1,8 @@
 using System.Windows;
 
+[assembly: System.Reflection.AssemblyFileVersion("0.0.9.0")]
+[assembly: System.Reflection.AssemblyVersion("0.9.0.0")]
+[assembly: System.Reflection.AssemblyProduct("WeThePeople_ModdingTool")]
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
