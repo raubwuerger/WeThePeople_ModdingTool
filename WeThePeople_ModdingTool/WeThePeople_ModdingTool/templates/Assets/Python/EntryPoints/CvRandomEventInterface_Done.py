fa0f386e708644e734585bf2a887ee6c60a58974
raubwuerger <hannuschka@web.de>
2022-09-22 19:29:25 +0200

    Correct event trigger done condition.

--- WeThePeople_ModdingTool/WeThePeople_ModdingTool/templates/Assets/Python/EntryPoints/CvRandomEventInterface_Done.py
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/templates/Assets/Python/EntryPoints/CvRandomEventInterface_Done.py
@@ -1,7 +1,7 @@
 def canTrigger$HARBOUR_NORMAL$TradeQuest_$YIELD$_DONE(argsList):
 	
 	# Read Parameters 1+2 from the two events and check if enough yield is stored in city
-	eEvent = gc.getInfoTypeForString("EVENT_$HARBOUR_UPPERCASE$_TRADE_QUEST_$YIELD$_DONE")
+	eEvent = gc.getInfoTypeForString("EVENT_$HARBOUR_UPPERCASE$_TRADE_QUEST_$YIELD$_DONE_1")
 	event = gc.getEventInfo(eEvent)
 	iYieldID = event.getGenericParameter(2)
 	iQuantity = event.getGenericParameter(1) # for Quest Done this should be e.g. 1000
