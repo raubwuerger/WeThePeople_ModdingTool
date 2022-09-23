56fe07e6eecbf354ed7208a0f951244d7d46c1c1
raubwuerger <hannuschka@web.de>
2022-09-23 10:11:52 +0200

    Added column domain.

--- WeThePeople_ModdingTool/WeThePeople_ModdingTool/Windows/GridControl.xaml.cs
+++ WeThePeople_ModdingTool/WeThePeople_ModdingTool/Windows/GridControl.xaml.cs
@@ -28,8 +28,10 @@ namespace WeThePeople_ModdingTool.Windows
         private readonly string Civ4UnitInfos_iPortRoyalCost = "iPortRoyalCost";
         private readonly string Civ4UnitInfos_bAnimal = "bAnimal";
         private readonly string Civ4UnitInfos_Special = "Special";
+        private readonly string Civ4UnitInfos_Domain = "Domain";
 
         private readonly string PREFIX_UNIT = "UNIT_";
+        private readonly string PREFIX_DOMAIN = "DOMAIN_";
         private readonly string NODE_NOT_FOUND = "NODE_NOT_FOUND";
         private readonly string SPECIALUNIT_YIELD_CARGO = "SPECIALUNIT_YIELD_CARGO";
 
@@ -65,15 +67,17 @@ namespace WeThePeople_ModdingTool.Windows
             sheet[0, 1] = "Europe";
             sheet[0, 2] = "Africa";
             sheet[0, 3] = "PortRoyal";
+            sheet[0, 4] = "Domain";
         }
         private void FillData()
         {
             var sheet = grid.CurrentWorksheet;
 
             List<string> units = new List<string>();
-            IDictionary<string, int> unitTypeEurope = new System.Collections.Generic.Dictionary<string, int>();
-            IDictionary<string, int> unitTypeAfrica = new System.Collections.Generic.Dictionary<string, int>();
-            IDictionary<string, int> unitTypePortRoyal = new System.Collections.Generic.Dictionary<string, int>();
+            IDictionary<string, int> costEuropeAll = new System.Collections.Generic.Dictionary<string, int>();
+            IDictionary<string, int> costAfricaAll = new System.Collections.Generic.Dictionary<string, int>();
+            IDictionary<string, int> costPortRoyalAll = new System.Collections.Generic.Dictionary<string, int>();
+            List<string> domainAll = new List<string>();
 
             XmlNodeList xmlNodeList = xmlDocument.DocumentElement.GetElementsByTagName(Civ4UnitInfos_UnitInfo);
             foreach (XmlNode xmlNode in xmlNodeList)
@@ -119,10 +123,13 @@ namespace WeThePeople_ModdingTool.Windows
                     costPortRoyal = 0;
                 }
 
+                string domain = GetSubnodeValue(xmlNode, Civ4UnitInfos_Domain);
+
                 units.Add(unitName);
-                unitTypeEurope.Add(unitName, costEurope);
-                unitTypeAfrica.Add(unitName, costAfrica);
-                unitTypePortRoyal.Add(unitName, costPortRoyal);
+                costEuropeAll.Add(unitName, costEurope);
+                costAfricaAll.Add(unitName, costAfrica);
+                costPortRoyalAll.Add(unitName, costPortRoyal);
+                domainAll.Add(domain);
             }
 
             int rowStartIndex = 1;
@@ -137,15 +144,16 @@ namespace WeThePeople_ModdingTool.Windows
             {
                 currentColumnIndex = columnStartIndex;
                 sheet[currentRowIndex, currentColumnIndex++] = RemoveFront(PREFIX_UNIT, units[index]);
-                sheet[currentRowIndex, currentColumnIndex++] = unitTypeEurope[units[index]];
-                sheet[currentRowIndex, currentColumnIndex++] = unitTypeAfrica[units[index]];
-                sheet[currentRowIndex, currentColumnIndex++] = unitTypePortRoyal[units[index]];
+                sheet[currentRowIndex, currentColumnIndex++] = costEuropeAll[units[index]];
+                sheet[currentRowIndex, currentColumnIndex++] = costAfricaAll[units[index]];
+                sheet[currentRowIndex, currentColumnIndex++] = costPortRoyalAll[units[index]];
+                sheet[currentRowIndex, currentColumnIndex++] = RemoveFront(PREFIX_DOMAIN, domainAll[index]);
 
-                if (unitTypeEurope[units[index]] == 0 && unitTypeAfrica[units[index]] == 0 && unitTypePortRoyal[units[index]] == 0)
+                if (costEuropeAll[units[index]] == 0 && costAfricaAll[units[index]] == 0 && costPortRoyalAll[units[index]] == 0)
                 {
-                    int europeCost = unitTypeEurope[units[index]];
-                    int africaCost = unitTypeAfrica[units[index]];
-                    int portRoyalCost = unitTypePortRoyal[units[index]];
+                    int europeCost = costEuropeAll[units[index]];
+                    int africaCost = costAfricaAll[units[index]];
+                    int portRoyalCost = costPortRoyalAll[units[index]];
 
                     var range = sheet.Ranges[currentRowIndex, columnStartIndex, 1, currentColumnIndex];
                     range.Style.BackColor = Color.FromRgb(211,211,211);
